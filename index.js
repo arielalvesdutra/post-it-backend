477b1321da28bc2cdce4d0bd1f5d0275d03e0e4e
arielalvesdutra <alves.lsn@gmail.com>
2019-05-21 17:31:13 -0300

    Adiciona regra para permitir qualquer método HTTP

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const postItController = require('./controllers/post-it-controller')
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
